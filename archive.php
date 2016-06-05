3b0e448eddeceb943fe78265eb7bf82dbd2fcd4b
p0ody <mcliche91@gmai.com>
2016-06-05 14:00:52 -0400

    Add async to loading of javascript file

--- archive.php
+++ archive.php
@@ -26,13 +26,13 @@ function hasSearch($search) { return $search !== false; }
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>FF2EBOOK :: Archive</title>
-    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.3.min.js"></script>
+    <script async src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.3.min.js"></script>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script async src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-    <script src="js/errorHandler.js"></script>
-    <script src="js/archive.js"></script>
+    <script async src="js/errorHandler.js"></script>
+    <script async src="js/archive.js"></script>
     <link rel="stylesheet" type="text/css" href="default.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -4,15 +4,15 @@
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>FF2EBOOK :: Download your favorites FanFictions as eBooks.</title>
-    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.3.min.js"></script>
+    <script async src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.3.min.js"></script>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script async src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-    <script src="js/errorHandler.js"></script>
-    <script src="js/functions.js"></script>
-    <script src="js/ajax.functions.js"></script>
-    <script src="js/main.js"></script>
+    <script async src="js/errorHandler.js"></script>
+    <script async src="js/functions.js"></script>
+    <script async src="js/ajax.functions.js"></script>
+    <script asyncsrc="js/main.js"></script>
     <link rel="stylesheet" type="text/css" href="default.css">
 </head>
 <body>
@@ -122,5 +122,6 @@
         </div>
 
     </div>
+
 </body>
 </html>
