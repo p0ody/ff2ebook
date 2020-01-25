8226cf7b2de5c86e57c65328132b5321ab013104
Maxime Cliche <mcliche91@gmail.com>
2020-01-25 09:13:13 -0500

    Edited cleaning scripts

--- scripts/cleanUnlinkedFiles.php
+++ scripts/cleanUnlinkedFiles.php
@@ -3,21 +3,48 @@ require_once("../class/class.FileManager.php");
 require_once("../class/class.dbHandler.php");
 
 // Delete files with no DB link.
-$fileList = scandir("../archive");
+/*$fileList = scandir("../archive");
 
 if (!$fileList)
     die("Error retrieving files list.");
+*/
 
 $db = new dbHandler();
 $pdo = $db->connect();
-$query = $pdo->prepare("SELECT `id` FROM `fic_archive`;");
+$query = $pdo->prepare("SELECT `id`,`site` FROM `fic_archive`;");
 $query->execute();
 
-$results = $query->fetchAll(PDO::FETCH_UNIQUE);
+$results = $query->fetchAll(PDO::FETCH_KEY_PAIR);
 
 $fm = new FileManager();
 
 $counter = 0;
+foreach (new DirectoryIterator("../archive") as $file)
+{
+    if ($file->getExtension() != "epub" && $file->getExtension() != "mobi")
+        continue;
+
+    $name = $file->getFilename();
+    if (preg_match("#([a-zA-Z]+)_([0-9]+)_[0-9]+#", $name, $matches) === 1)
+    {
+        if (!array_key_exists($matches[2], $results))
+        {
+            $fm->deleteFile($file->getPathname());
+            echo $name ." deleted. <br />";
+            $counter++;
+        }
+        else
+        {
+            if ($results[$matches[2]] == $matches[1])
+                continue;
+
+            $fm->deleteFile($file->getPathname());
+            echo $name ." deleted. <br />";
+            $counter++;
+        }
+    }
+}
+/*
 foreach ($fileList as $file)
 {
     if (preg_match("#[a-zA-Z]+_([0-9]+)_[0-9]+#", $file, $matches) === 1)
@@ -30,5 +57,5 @@ foreach ($fileList as $file)
         }
     }
 }
-
+*/
 echo $counter ." files deleted.";
