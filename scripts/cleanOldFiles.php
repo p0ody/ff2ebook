6afbb7e7d2cb73b2067337e6c09a979fb9fe8f2e
Maxime Cliche <mcliche91@gmail.com>
2020-01-24 20:54:50 -0500

    Finalized cleaning script

--- scripts/cleanOldFiles.php
+++ scripts/cleanOldFiles.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("../class/class.dbHandler.php");
+require_once("../class/class.FileManager.php");
 
 try
 {
@@ -11,11 +12,41 @@ try
     $query = $pdo->prepare("DELETE FROM `fic_archive` WHERE `lastDL` < ". (time() - $sixMonths) .";");
     $query->execute();
 
-    die($query->rowCount() ." rows deleted.");
+    echo $query->rowCount() ." rows deleted in database. <br />";
+
+    // Delete files with no DB link.
+    $fileList = scandir("../archive");
+
+    if (!$fileList)
+        die("Error retrieving files list.");
+       
+    $query = $pdo->prepare("SELECT `id` FROM `fic_archive`;");
+    $query->execute();
+
+    $results = $query->fetchAll(PDO::FETCH_UNIQUE);
+
+    $fm = new FileManager();
+
+    $counter = 0;
+    foreach ($fileList as $file)
+    {
+        if (preg_match("#[a-zA-Z]+_([0-9]+)_[0-9]+#", $file, $matches) === 1)
+        {
+            if (!array_key_exists($matches[1], $results))
+            {
+                $fm->deleteFile("../archive/". $file);
+                echo $file ." deleted. <br />";
+                $counter++;
+            }
+        }
+    }
+
+    echo $counter ." files deleted.";
+    
+
 }
 catch(PDOException $e)
 {
     die("An error has occured.");
     
-
 }
