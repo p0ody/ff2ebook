f8bf11eddc13d0d92a6cde32e80491f2e4d6e119
Maxime Cliche <mcliche91@gmail.com>
2020-02-22 08:57:26 -0500

    Fixed dir reference in scripts

--- scripts/cleanDB.php
+++ scripts/cleanDB.php
@@ -1,13 +1,15 @@
 <?php
 
-require_once("../class/class.dbHandler.php");
+require_once(__DIR__ ."/../class/class.dbHandler.php");
+
+// Delete files that have not been downloaded for 2 Months
 
 try
 {
     $db = new dbHandler();
     $pdo = $db->connect();
 
-    $sixMonths = 6/*Months*/ * 30 /*Days*/ * 24 /*Hours*/ * 60 /*Minutes*/ * 60 /*Seconds*/;
+    $sixMonths = 2/*Months*/ * 30 /*Days*/ * 24 /*Hours*/ * 60 /*Minutes*/ * 60 /*Seconds*/;
     $query = $pdo->prepare("DELETE FROM `fic_archive` WHERE `lastDL` < ". (time() - $sixMonths) .";");
     $query->execute();
 
--- scripts/cleanUnlinkedFiles.php
+++ scripts/cleanUnlinkedFiles.php
@@ -1,6 +1,6 @@
 <?php
-require_once("../class/class.FileManager.php");
-require_once("../class/class.dbHandler.php");
+require_once(__DIR__ ."/../class/class.FileManager.php");
+require_once(__DIR__ ."/../class/class.dbHandler.php");
 
 // Delete files with no DB link.
 /*$fileList = scandir("../archive");
