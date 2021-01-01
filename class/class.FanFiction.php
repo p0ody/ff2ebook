5cb6871ecc0ed11b62db9f1a8169c76c073ab3e8
Bastien Saidi <saidi.ireland@gmail.com>
2021-01-01 23:04:19 +0000

    maybe found issue

--- class/class.FanFiction.php
+++ class/class.FanFiction.php
@@ -10,7 +10,7 @@ function bypass_cf($url="null"){ //added function to pass requests to python.
     if ($url == "null"){
         return;
     }
-    $command = '/bin/bash -c \'python3 ../class/py/cf_curl.py '.$url." 2>&1;echo hey;'";
+    $command = '/bin/bash -c \'source /home/bastyoung/.bashrc;python3 ../class/py/cf_curl.py '.$url." 2>&1;echo hey;'";
     $source = shell_exec($command);
     echo shell_exec("pwd")."     ";
     echo $source."     ";
