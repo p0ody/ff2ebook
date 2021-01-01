e0b788dd219db683e76ad64137af13c14bac8318
Bastien Saidi <saidi.ireland@gmail.com>
2021-01-01 22:18:24 +0000

    Small typo

--- class/class.FanFiction.php
+++ class/class.FanFiction.php
@@ -10,7 +10,7 @@ function bypass_cf($url="null"){ //added function to pass requests to python.
     if ($url == "null"){
         return;
     }
-    $command = 'python3 py/cf_curl.py '+$url;
+    $command = 'python3 py/cf_curl.py '.$url;
     return shell_exec($command);
 }
 
