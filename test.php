d8cb0f1af5f3d919698190f89db413c98d86ac04
Maxime Cliche <mcliche91@gmail.com>
2021-09-26 14:05:27 -0400

    Forgot to remove that

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__."/class/class.SourceHandler.php";
-require_once __DIR__."/class/class.ProxyManager.php";
-
-$source = SourceHandler::useSelenium("https://m.fanfiction.net/s/2651376/1/Voldemort-s-Last-Spell");
-
-echo $source;
