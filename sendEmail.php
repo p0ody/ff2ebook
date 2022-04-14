e7db31d5141003a574b01e9bd0b0885b924c562b
Maxime Cliche <mcliche91@gmail.com>
2022-04-13 20:08:16 -0400

    Added headers to a seperate file to help with updates across multiple pages

--- sendEmail.php
+++ sendEmail.php
@@ -49,29 +49,12 @@ catch (Exception $e) {
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>FF2EBOOK :: Contact</title>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131272468-1"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-131272468-1');
-    </script>
-
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js"
-        integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
-        crossorigin="anonymous">
-    </script>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-    <link rel="stylesheet" type="text/css" href="default.css">
+    
+    <?php include("html/header.html") ?>
 </head>
 <body>
     <div class="container-fluid">
-        <?php include("menu.html") ?>
+        <?php include("html/menu.html") ?>
         <!-- Input -->
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
